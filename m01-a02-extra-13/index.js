4b9703484987000abc3652c66143ef14782a68ef
mv-muniz <celomark_18@hotmail.com>
2021-03-19 23:01:52 -0300

    Exercicio 13 resolvido

---
+++ m01-a02-extra-13/index.js
@@ -0,0 +1,7 @@
+C = 0, A = 0, r = 5;  // C = Cumprimento da circunferência - A  = área da círculo - r = raio
+
+A = Math.PI * Math.pow(r, 2);
+C = 2 * Math.PI * r;
+console.log(`A área deste círculo é ${A} cm²`);
+console.log(`O cumprimento deste círculo é ${C} cm`);
+

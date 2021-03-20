70ad6b66e114b6509dc514dd25d0998876860c5d
nocrag <joaoggr@hotmail.com>
2021-03-19 22:09:45 -0300

    removido exercicio que estava na pasta errada

--- m01-a02-classe-07/index.js
+++
@@ -1,8 +0,0 @@
-let valorProduto = 129, precoFinal = 80;
-
-let desconto = valorProduto - precoFinal
-desconto = 100 * desconto
-desconto = Math.round(desconto / valorProduto)
-
-
-console.log(`O desconto necessario para a compra do tenis e de ${desconto}%`);

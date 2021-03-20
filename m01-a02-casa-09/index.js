00d8f5af51c0cbe24997a14028dee7d05239ab46
Charles Roberto <charlesrobertolima@gmail.com>
2021-03-20 15:15:59 -0300

    Exercício 09 da aula 02 resolvido

---
+++ m01-a02-casa-09/index.js
@@ -0,0 +1,16 @@
+let M = 0;
+let C = 1000;
+let i = 12.5/100;
+let t = 5;
+let z = 0;
+let p = 0;
+
+z = 1 + i;
+p= Math.pow(z, t);
+M = C * p;
+console.log(`O Valor do Montante Conforme o Input Recebido é de R$ ${M.toFixed(2)}`); 
+
+
+
+
+

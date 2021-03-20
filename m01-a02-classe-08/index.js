45bf2d1801f8ac9879322fb07d887360ac956aeb
Icaro Assis <icaroassisfreitas@globo.com>
2021-03-19 21:22:47 -0300

    Resolução exercício 8

---
+++ m01-a02-classe-08/index.js
@@ -0,0 +1,6 @@
+const precoTenis = 130;
+const valorDesconto = 50;
+
+const porcentDesconto = ((valorDesconto*100)/precoTenis);
+
+console.log(`A porcentagem do desconto terá que ser de ${porcentDesconto.toFixed(1)}%`);

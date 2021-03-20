a46bb5a0825dfdad46d0e01bf5ac9d7a60c82c07
euritales <58851676+euritales@users.noreply.github.com>
2021-03-19 21:46:52 -0300

    Resolucao classe

---
+++ m01-a02-classe-07/index.js
@@ -0,0 +1,4 @@
+let grausF = 73;
+let grausC = (grausF - 32) * 5/9;
+
+console.log (`Temperatura convertida para: ${grausC}ºC`);
---
+++ m01-a02-classe-08/index.js
@@ -0,0 +1,5 @@
+let dinheiro = 80, tenis = 129.99, diferenca = 49.99, cupom = 0;
+
+cupom = (tenis * diferenca) / dinheiro;
+
+console.log(`Valor do Cupom: ${cupom}`);

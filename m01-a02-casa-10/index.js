cec179c4d7a2166b6299efbd6a920856caa2ef89
Icaro Assis <icaroassisfreitas@globo.com>
2021-03-20 14:17:17 -0300

    Correção das variáveis no exercício 10

--- m01-a02-casa-10/index.js
+++ m01-a02-casa-10/index.js
@@ -1,8 +1,8 @@
-const x1 = 1;
-const x2 = 1;
-const y1 = 3;
-const y2 = 5;
+let x1 = 1;
+let x2 = 1;
+let y1 = 3;
+let y2 = 5;
 
-distancia = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+const distancia = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
 
 console.log(`d = ${distancia}`);

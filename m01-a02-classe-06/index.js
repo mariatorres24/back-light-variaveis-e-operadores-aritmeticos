23b22dd96d4cfa741448ff294982fc12ad342119
opedroaravena <pedroaravena.contato@gmail.com>
2021-03-20 17:35:37 -0300

    adicionando estilo

--- m01-a02-classe-06/index.js
+++ m01-a02-classe-06/index.js
@@ -14,13 +14,13 @@ function calculaImc(peso, altura){
     if(imc <= 16 && imc < 17){
         resposta = (`Você está MUITO abaixo do peso, ${nome}. Seu IMC é de ${parseInt(imc)}`);
     } else if(imc >= 17 && imc < 18){
-        resposta = (`Você está um pouco abaixo do peso,${nome} seu IMC é de ${parseInt(imc)}`);
+        resposta = (`Você está um pouco abaixo do peso, ${nome}. Seu IMC é de ${parseInt(imc)}`);
     } else if(imc >= 18 && imc < 25){
-        resposta = (`Você está no peso ideal,${nome} seu IMC é de ${parseInt(imc)}`);
+        resposta = (`Você está no peso ideal, ${nome}. Seu IMC é de ${parseInt(imc)}`);
     } else if(imc >= 25 && imc <= 30){
-        resposta = (`Você está acima do peso,${nome} seu IMC é de ${parseInt(imc)}`);
+        resposta = (`Você está acima do peso, ${nome}. Seu IMC é de ${parseInt(imc)}`);
     } else {
-        resposta = (`Você está MUITO acima do peso,${nome} seu IMC é de ${parseInt(imc)}`);
+        resposta = (`Você está MUITO acima do peso, ${nome}. Seu IMC é de ${parseInt(imc)}`);
     }
     
 

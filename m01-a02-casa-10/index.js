b50c263f201b6ddc0f02de862701d3ca8b209902
Estefane <estefanecortes@gmail.com>
2021-03-20 18:23:05 -0300

    Respondendo atividade para casa 10

---
+++ m01-a02-casa-10/index.js
@@ -0,0 +1,7 @@
+const x1 = 1, x2 = 1; 
+const y1 = 1, y2 = 4; 
+
+//separando em dois para ficar mais visivel
+let distancia = Math.pow((x2-x1),2)+Math.pow((y2-y1),2); 
+distancia = Math.sqrt(distancia);
+console.log(distancia);

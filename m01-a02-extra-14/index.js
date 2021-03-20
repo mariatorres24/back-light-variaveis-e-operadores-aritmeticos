3d1d30b6a2a9fd35d3620227940bce984d2ca1c9
mv-muniz <celomark_18@hotmail.com>
2021-03-19 23:07:42 -0300

    Exercicio 14 resolvido

---
+++ m01-a02-extra-14/index.js
@@ -0,0 +1,4 @@
+let s = 0, n = 6;           //s = soma dos ângulos internos de um polígono - n = nº de lados deste polígono
+
+s = (n - 2) * 180
+console.log(`A soma dos ângulos internos de um polígono de ${n} lados é ${s}º`);

8e3825367a0b26dda53ae88397b02afe9c02d35c
ilwel <ilwelisaac@hotmail.com>
2021-03-20 13:18:23 -0300

    troquei as atividades sem querer, atividade de classe 1

---
+++ m01-a02-classe-06/index.js
@@ -0,0 +1,11 @@
+//na pressa eu troquei as atividades
+
+//vou fazer as de classe agora
+
+function calcImc(peso, altura){
+
+    return peso / (Math.pow(altura, 2))
+
+}
+
+console.log(calcImc(76, 1.79))

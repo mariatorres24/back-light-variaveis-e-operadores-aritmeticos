04cefc240d1eed6ea5cb57ce781e04d842f143df
Tiago Nunes <69474740+nunestiago@users.noreply.github.com>
2021-03-20 15:40:23 -0300

    Fixed the pow

--- m01-a02-extra-12/index.js
+++ m01-a02-extra-12/index.js
@@ -1,7 +1,7 @@
 const Po = 1000;
 const x = 4;
 const t = 100;
-let P = (Po * x) ^ (t / 7);
+let P = (Po * x) ** (t / 7);
 
 console.log(
   `Se ${Po} pessoas forem infectadas, em 100 dias, será transmitido para ${P}`

1e34c224e47eeac2a943e4e22ed0b19ce1c9206e
Ticiano Lino <ticianolino@MacTicken.local>
2021-03-20 13:35:29 -0300

    resoluçao atividade casa 10

---
+++ m01-a02-casa-10/index.js
@@ -0,0 +1,20 @@
+
+
+// let x1 = 1
+// let y1 = 1
+// let x2 = 1
+// let y2 = 4
+// const distância = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+
+// console.log(distância);
+
+
+
+let x1 = 1
+let y1 = 1
+let x2 = 1
+let y2 = 4
+const distância = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+
+console.log(distância);
+

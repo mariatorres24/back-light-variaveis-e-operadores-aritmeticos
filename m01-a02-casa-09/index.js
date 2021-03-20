0c54ea1081b0fbaf93bd90a5e7611481acfc3517
Icaro Assis <icaroassisfreitas@globo.com>
2021-03-20 14:03:49 -0300

    Remove o prompt da resolução do exercício 9

--- m01-a02-casa-09/index.js
+++ m01-a02-casa-09/index.js
@@ -1,14 +1,11 @@
-// Como metódo de entrada usei o antiquado 'prompt'
-// Assim o código opera somente rodando no console dos navegadores e não no node.
-
 console.log("Bem-vindo a calculadora de juros compostos!");
 
-const capital = prompt("Insira o valor do seu capital:");
-const taxa = prompt("Insira o valor da porcentagem da taxa mensal fixa:")
-const meses = prompt("Insira o valor referente a quantidade de meses que quer investir");
+const capital = 1000
+const taxa = 12.5
+const meses = 5
 
 const taxaDec = taxa/100;
 
-const montante = capital*((1 + taxaDec)**meses);
+const montante = capital*(Math.pow((1 + taxaDec), meses));
 
 console.log(`O seu montante investindo $${capital} durante ${meses} mes(es) com uma taxa de ${taxa}% é de $${montante.toFixed(0)}.`);

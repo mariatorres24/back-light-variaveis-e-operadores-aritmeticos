bf5049ca85bd938ee686500758229ee8e2d28d46
Ticiano Lino <ticianolino@MacTicken.local>
2021-03-20 14:24:55 -0300

    resoluçao atividade extra 11

---
+++ m01-a02-extra-11/index.js
@@ -0,0 +1,8 @@
+let distPercorridaEmMetros = 1_000;
+let tempoEmSegundos = 60;
+
+const velocidadeEmMpS = distPercorridaEmMetros / tempoEmSegundos
+const velocidadeEmKmpH = velocidadeEmMpS * 3.6
+
+
+console.log(velocidadeEmKmpH);

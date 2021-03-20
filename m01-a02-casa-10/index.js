62f72123ae033c8e45737d5d1b6b26afad2a77c7
Felipe Belisário <fbelisario@gmail.com>
2021-03-19 21:14:34 -0300

    Exercício 10 (Calcular a distância entre dois pontos) resolvido

---
+++ m01-a02-casa-10/index.js
@@ -0,0 +1,3 @@
+let x1 = 1, x2 = 1, y1 = 1, y2 = 4;
+let d = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+console.log(`Para as entradas de x1 = ${x1}, x2 = ${x2}, y1 = ${y1} e y2 = ${y2} o valor de d é ${d}.`);

e7ea0177f5151764a53134badd1408dec03688c8
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 09:13:49 -0300

    Exercício casa 10 feito

---
+++ m01-a02-casa-10/index.js
@@ -0,0 +1,9 @@
+let pontoX1 = 1;
+let pontoY1 = 1;
+
+let pontoX2 = 1;
+let pontoY2 = 4;
+
+let distanciaEntrePontos = Math.sqrt(Math.pow((pontoX2 - pontoX1), 2) + Math.pow((pontoY2 - pontoY1), 2))
+
+console.log(`A distância entre as coordenadas X e Y é ${distanciaEntrePontos}.`);

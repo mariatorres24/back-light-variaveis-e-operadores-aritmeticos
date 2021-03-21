01bcc5df429b50790b90599379028f569748138a
VhHonorato <vhhonorato@hotmail.com>
2021-03-21 20:47:42 -0300

    Mais 2 exemplos

--- m01-a02-classe-06/index.js
+++ m01-a02-classe-06/index.js
@@ -1,7 +1,14 @@
-let altura = 1.84, peso = 75;
+let altura = 1.84;
+let peso = 75;
 let imc = peso / Math.pow(altura,2);
 console.log(`Meu IMC é ${imc}`);
 
-altura = 1.79
-peso = 76
-console.log(imc);
+altura = 1.79;
+peso = 76;
+imc = peso / Math.pow(altura,2);
+console.log(`O IMC do exemplo do readme é ${imc}`);
+
+altura = 1.51;
+peso = 82;
+imc = peso / Math.pow(altura,2);
+console.log(`Mais um exemplo de IMC é ${imc}`);

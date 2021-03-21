7519e931b1e26897d2ba7cbe77971e12a71f77fc
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 13:02:45 -0300

    Exercício extra 13 feito

---
+++ m01-a02-extra-13/index.js
@@ -0,0 +1,5 @@
+let raioCirculo = 10;
+let comprimentoCirculo = (2 * 3.14 * raioCirculo).toFixed(2);
+let areaCirculo = (3.14 * Math.pow(raioCirculo, 2)).toFixed(2);
+
+console.log(`Tendo um círculo de raio ${raioCirculo}, seu comprimento e sua área serão ${comprimentoCirculo} e ${areaCirculo} respectivamente.`);

7ba3ba95002e8a3876bdf8568149d22361967212
afonsocgl <afonsocgl@gmail.com>
2021-03-21 16:32:48 -0300

    Exercício de Classe 07 com uso da Função Math.roud

--- m01-a02-classe-06/index.js
+++ m01-a02-classe-06/index.js
@@ -1,6 +1,6 @@
 let peso = 76, altura = 1.79, IMC = 0;
 
-IMC = peso / (Math.pow (altura, 2));
+IMC = peso / (Math.pow (altura, 2)) ;
 
 IMC = IMC.toFixed(1);
 
---
+++ m01-a02-classe-07/index.js
@@ -0,0 +1,5 @@
+let f = 73, c = 0;
+
+c = Math.round ((f-32) * (5/9));
+
+console.log (c);

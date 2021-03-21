1caa1db0670ae312275722e511309e500bf4353a
Charles Roberto <charlesrobertolima@gmail.com>
2021-03-21 10:06:23 -0300

    Exercício 13 da aula 02 resolvido

---
+++ m01-a02-extra-13/index.js
@@ -0,0 +1,8 @@
+let C = 0;
+let Ao = 0;
+let raio = 5;  
+
+Ao = Math.PI * Math.pow(raio, 2);
+C = 2 * Math.PI * raio;
+console.log(`A Área do círculo é de ${Ao.toFixed(2)} cm²`);
+console.log(`O Cumprimento do círculo é de ${C.toFixed(2)} cm`)

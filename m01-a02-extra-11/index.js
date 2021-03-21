52e5a800ba72b398f702a8dae5655699027eb078
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 09:31:59 -0300

    Exercício extra 11 feito

---
+++ m01-a02-extra-11/index.js
@@ -0,0 +1,6 @@
+let distanciaEmMetros = 1000;
+let tempoEmSegundos = 250;
+
+let velocidadeMediaEmKmH = ((distanciaEmMetros / tempoEmSegundos) * 3.6).toFixed(1);
+
+console.log(`Percorrendo ${distanciaEmMetros} metros em ${tempoEmSegundos} segundos, sua velocidade média será de ${velocidadeMediaEmKmH}Km/h`);

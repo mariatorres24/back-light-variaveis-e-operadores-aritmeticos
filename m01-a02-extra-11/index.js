5cc2651164abef21e6dabd090a62f7a23bfb1e38
Charles Roberto <charlesrobertolima@gmail.com>
2021-03-21 09:30:19 -0300

    Exercício 11 da aula 02 resolvido

---
+++ m01-a02-extra-11/index.js
@@ -0,0 +1,8 @@
+let vel = 0;
+let tempo = 60;
+let distânciaPerc = 7.42;
+let fatConversao = 3.6;
+
+vel = distânciaPerc / tempo;
+vel = vel * fatConversao;
+console.log("A Velocidade Média percorrida é de " + vel + " Km/h ")

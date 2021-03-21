1bda831ef832576dd0749e8dae919aa0acd9e537
afonsocgl <afonsocgl@gmail.com>
2021-03-21 17:47:04 -0300

    Exercício Extra 11

---
+++ m01-a02-extra-11/index.js
@@ -0,0 +1,6 @@
+let velocidade = 0, distPercorrida = 5000, tempo = 1800, fatorConv = 3.6;
+
+velocidade = (distPercorrida / tempo) * fatorConv;
+
+console.log (`Velocidade média ${velocidade}km/h`); 
+

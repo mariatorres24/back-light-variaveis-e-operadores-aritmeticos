a5374ed834d7a65c7afede0c56eec339e3cd770e
Felipe Belisário <fbelisario@gmail.com>
2021-03-21 20:33:50 -0300

    Exercício 11 (Extra - Calcular a velocidade de uma pessoa) resolvido

---
+++ m01-a02-extra-11/index.js
@@ -0,0 +1,4 @@
+let distPercorrida = 5_000;
+let tempo = 2_400;
+let velocidade = distPercorrida / tempo * 3.6;
+console.log(`Ao percorrer ${distPercorrida}m em ${tempo}s, a velocidade média é de ${velocidade.toFixed(1)}km/h.`)

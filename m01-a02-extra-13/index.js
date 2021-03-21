26ca9bcb2d2570481e2bd4cd51accd126805419e
afonsocgl <afonsocgl@gmail.com>
2021-03-21 18:17:32 -0300

    Exercício Extra 13, usando as funções Math.PI e .toFixed

---
+++ m01-a02-extra-13/index.js
@@ -0,0 +1,10 @@
+let C = 0, Ao = 0, R = 10;
+
+C = 2 * Math.PI * R;
+Ao = Math.PI * Math.pow (R, 2);
+
+console.log (`O comprimento da circunferência é ${C.toFixed(2)}`);
+console.log (`A área da circuferência é ${Ao.toFixed(2)}`);
+console.log (`O valor aproximado de PI é ${Math.PI.toFixed(2)}`);
+
+

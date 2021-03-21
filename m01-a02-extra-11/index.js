129cac5887f3cea3e6144d58592edecb1e83242a
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 14:05:07 -0300

    toFixed(1) mudado para toFixed(2)

--- m01-a02-extra-11/index.js
+++ m01-a02-extra-11/index.js
@@ -1,6 +1,6 @@
 let distanciaEmMetros = 1000;
 let tempoEmSegundos = 250;
 
-let velocidadeMediaEmKmH = ((distanciaEmMetros / tempoEmSegundos) * 3.6).toFixed(1);
+let velocidadeMediaEmKmH = ((distanciaEmMetros / tempoEmSegundos) * 3.6).toFixed(2);
 
 console.log(`Percorrendo ${distanciaEmMetros} metros em ${tempoEmSegundos} segundos, sua velocidade média será de ${velocidadeMediaEmKmH}Km/h`);

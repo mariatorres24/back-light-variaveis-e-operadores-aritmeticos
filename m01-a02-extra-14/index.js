ebf06b2cb455b42237a694c176ec66152b55fbfc
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 13:15:57 -0300

    Exercício extra 14 feito

---
+++ m01-a02-extra-14/index.js
@@ -0,0 +1,5 @@
+let numeroLados = 4;
+let somaAngulos = (numeroLados - 2) * 180;
+let valorAngulo = somaAngulos / numeroLados;
+
+console.log(`Tendo um polígono de ${numeroLados} lados, a soma de seus ângulos internos é ${somaAngulos} e cada ângulo interno vale ${valorAngulo} graus.`)

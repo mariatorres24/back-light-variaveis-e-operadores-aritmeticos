c2659b6f6bdb332442732cb183a0cd50a8a76ad6
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 12:56:15 -0300

    .toFixed() adicionado. Mensagem do retorno modificada

--- m01-a02-classe-06/index.js
+++ m01-a02-classe-06/index.js
@@ -1,5 +1,5 @@
 let pesoEmQuilos = 50;
-let alturaEmMetros = 1.60;
-let indiceMassaCorporal = pesoEmQuilos / Math.pow(alturaEmMetros, 2);
+let alturaEmMetros = 1.80;
+let indiceMassaCorporal = (pesoEmQuilos / Math.pow(alturaEmMetros, 2)).toFixed(2);
 
-console.log(indiceMassaCorporal);
+console.log(`Seu peso é ${pesoEmQuilos}kg e sua altura ${alturaEmMetros} metros, logo seu IMC é ${indiceMassaCorporal}`);

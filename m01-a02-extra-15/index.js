43b14712ad557bf68ce0ec5b23c5609ad4e9a3c7
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 13:53:41 -0300

    Exercício extra 15 feito

---
+++ m01-a02-extra-15/index.js
@@ -0,0 +1,6 @@
+let coeficienteXQuadrado = - 4;
+let coeficienteX = 2;
+let coeficienteLivre = 2;
+let delta = Math.pow(coeficienteX, 2) - 4 * coeficienteXQuadrado * coeficienteLivre;
+
+console.log(`O valor do delta da fórmula ${coeficienteXQuadrado}xx + ${coeficienteX}x + ${coeficienteLivre} é ${delta}.`)

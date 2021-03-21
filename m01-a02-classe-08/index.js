f0336a52388496d7f467c9b8937f9e330e95e27c
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 09:11:00 -0300

    Trocando const por let (vai que quero comprar outro tênis)

--- m01-a02-classe-08/index.js
+++ m01-a02-classe-08/index.js
@@ -1,5 +1,5 @@
-const valorTenis = 129.99;
-const dinheiroNaMao = 80;
-const desconto = Math.round (100 - ((dinheiroNaMao * 100) / valorTenis));
+let valorTenis = 129.99;
+let dinheiroNaMao = 80;
+let desconto = Math.round (100 - ((dinheiroNaMao * 100) / valorTenis));
 
 console.log(`Preciso de ${desconto}% de desconto para comprar o tênis`);

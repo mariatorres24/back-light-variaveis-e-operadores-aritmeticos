ab4b79583f30da9429591fa1600dd4973c27d06b
Maylla Rabay <mayllarabay@gmail.com>
2021-03-21 14:09:24 -0300

    Valor das coordenadas adicionadas no retorno

--- m01-a02-casa-10/index.js
+++ m01-a02-casa-10/index.js
@@ -6,4 +6,4 @@ let pontoY2 = 4;
 
 let distanciaEntrePontos = Math.sqrt(Math.pow((pontoX2 - pontoX1), 2) + Math.pow((pontoY2 - pontoY1), 2))
 
-console.log(`A distância entre as coordenadas X e Y é ${distanciaEntrePontos}.`);
+console.log(`A distância entre as coordenadas (${pontoX1}, ${pontoY1}) e (${pontoX2}, ${pontoY2}) é ${distanciaEntrePontos}.`);

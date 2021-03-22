da03776253e35690d048b55bfa55f75249e62c1f
Danilo Tavares <stavaresdanilo@gamil.com>
2021-03-22 16:07:20 -0300

    Resolvido exercício 07

---
+++ m01-a02-classe-07/index.js
@@ -0,0 +1,5 @@
+let f = 73; // Declara a temperatura que se deseja converter em fahrenheit
+
+c = (f-32)*(5/9); // Converte a temperatura para Celsius e atribui a variável "c"
+
+console.log (c); // Imprime para o usuário a temperatura convertida

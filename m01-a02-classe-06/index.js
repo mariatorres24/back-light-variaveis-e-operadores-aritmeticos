4a90a12036761fb8002be27d4767fb3138293ae8
Danilo Tavares <stavaresdanilo@gamil.com>
2021-03-22 16:03:18 -0300

    Resolvido exercício 06

---
+++ m01-a02-classe-06/index.js
@@ -0,0 +1,6 @@
+let p = 76; // Declara peso da pessoa em kg
+const a = 1.79 // Declara a altura da pessoa em metros
+
+imc = p / Math.pow(a,2);  // Calcula o imc da pessoa
+
+console.log (imc); // Imprime o peso da pessoa

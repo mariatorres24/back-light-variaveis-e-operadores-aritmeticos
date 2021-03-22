89f9ba04e95fdcbcb799a8edf4dbb5d2d0384591
unknown <pedroforbrig@gmail.com>
2021-03-22 18:21:24 -0300

    Resolução Exercício 11

--- m01-a02-casa-10/index.js
+++ m01-a02-casa-10/index.js
@@ -3,4 +3,4 @@ let x2 = 1
 let y1 = 1
 let y2 = 4
 d = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2))
-console.log(`A distância entre x1,y1 e x2,y2 é ${d}`)
+console.log(`A distância entre (x1,y1) e (x2,y2) é ${d}.`)
---
+++ m01-a02-extra-11/index.js
@@ -0,0 +1,4 @@
+let d = 1000 // valor exemplo
+let t = 600 // valor exemplo
+V = (d/t) * 3.6
+console.log(`A velocidade média é ${V} km/h.`)

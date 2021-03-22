55c64d9ca4da2aaaf81fce3cdcfc61a112feb3b5
VhHonorato <vhhonorato@hotmail.com>
2021-03-21 21:37:35 -0300

    exercasa10 Distancia entre Pontos

---
+++ m01-a02-casa-10/index.js
@@ -0,0 +1,6 @@
+//![d = \sqrt{(x2 - x1)^2 + (y2 - y1)^2}](https://i.imgur.com/EIXmgc9.png)
+
+let x1 = 1, x2 = 1, y1 = 1, y2 = 4;
+let d = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+console.log(d);
+

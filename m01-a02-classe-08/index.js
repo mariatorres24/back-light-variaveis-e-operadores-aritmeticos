9a70018ada6cf177dd4abe231e4d6271122ab65d
unknown <caio.curvello@gmail.com>
2021-03-22 20:18:32 -0300

    Resolução exercicio 08

---
+++ m01-a02-classe-08/index.js
@@ -0,0 +1,5 @@
+let valorDoTenis = 129.99;
+let meuDinehiro = 80;
+let descontoEmReais = valorDoTenis - meuDinehiro;
+let x = "O desconto que preciso é de" + " " + (descontoEmReais / valorDoTenis) * 100 + " " + "porcento";
+console.log(x);

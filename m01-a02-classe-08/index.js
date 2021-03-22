f1ea3235346fb42a07d76243f8696a9953cbb95a
Danilo Tavares <stavaresdanilo@gamil.com>
2021-03-22 16:17:11 -0300

    Resolvido exercício 08

---
+++ m01-a02-classe-08/index.js
@@ -0,0 +1,8 @@
+// Basta calcular quanto equivale o valor a diferença (129,99-80) no total
+
+tenis =129.99; // Declara valor do tenis em reais
+carteira = 80.0; // Declara o valor que possui na carteira em reais
+
+desconto = ((tenis-carteira)/tenis)*100;// Calcula o desconto necessário para efetuar a compra
+
+console.log (desconto); // Imprime na tela o valor do desconto

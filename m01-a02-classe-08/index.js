3ee50952865e57d9581cc05162cbf66377e6a05a
Pedro Avena <pedroavena55@gmail.com>
2021-03-22 16:35:20 -0300

    exercício 08

---
+++ m01-a02-classe-08/index.js
@@ -0,0 +1,4 @@
+let orçamentoEmReais = 80;
+let preçoEmReais = 129.99;
+let descontoNecessárioEmReais = preçoEmReais -  orçamentoEmReais;
+console.log(`O desconto necessário para compra do tênis seria de ${descontoNecessárioEmReais * 100 / preçoEmReais} porcento.`)

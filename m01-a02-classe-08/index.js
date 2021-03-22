1b0bdcafd11a7645df654171ed411cda8e82bf17
DESKTOP-BQL5J4Q\gferr <gferrariemc@gmail.com>
2021-03-21 22:22:37 -0300

    resposta exercicio 08

---
+++ m01-a02-classe-08/index.js
@@ -0,0 +1,9 @@
+let cash=80, tenis=129.99, diferença=0, desconto =0;
+
+diferença= tenis - cash; 
+
+desconto= diferença/tenis;
+
+desconto= desconto*100
+
+console.log(`O desconto percentual para ele conseguir comprar o tênis é ${desconto}%`);

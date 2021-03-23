e817eb4ed43bd71fef9362792af7df2235729c5b
Leonardo Lacerda <Leonardo.Lacerda@cineplex.com>
2021-03-23 19:16:13 -0400

    Atividade de Classe 07 concluída

--- m01-a02-classe-06/index.js
+++ m01-a02-classe-06/index.js
@@ -1,6 +1,7 @@
 const A = 1.80; P = 99;
 
 let I = P/(A*A);
+//I = \frac{P}A²};
 
 
 console.log(`O IMC de qem tem altura ${A} e peso ${P} é: ${I}`);
---
+++ m01-a02-classe-07/index.js
@@ -0,0 +1,5 @@
+const F = 85; 
+
+const C = (F - 32) * (5/9);
+
+console.log(`${F} graus fahrenheit equivale a ${C} graus celsius`);

de9a2dbd6820e349a269245f0d4421b74ea40dd8
Danilo Tavares <stavaresdanilo@gamil.com>
2021-03-24 20:36:52 -0300

    Realizado exercício 11

---
+++ m01-a02-extra-11/index.js
@@ -0,0 +1,8 @@
+let distPercorrida = 5000; // em metros
+let tempo = 1800; // em segundos
+
+velocidade = distPercorrida/tempo; // m/s
+
+velocidadeconvert = velocidade*3.6;
+
+console.log(velocidadeconvert);

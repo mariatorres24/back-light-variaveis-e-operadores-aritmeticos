9beeabbb27d57b57d65889bcb8f1bb40b4181678
Lucas Prates <lucaspsanta@gmail.com>
2021-03-24 12:47:59 -0300

    Resolução do Exercício 10 - Distance between two Points

--- m01-a02-casa-10/index.js
+++ m01-a02-casa-10/index.js
@@ -6,7 +6,7 @@ let x1 = 1, y1 = 1; //coordenates of the 1st point
 let x2 = 1, y2 = 4; //coordenates of the 2nd point
 
 //Formula of the distance between 2 points : D
-let D = Math.sqrt ( Math.pow((x2-x1),2) + Math.pow(y2-y1),2));
+let D = Math.sqrt ( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
 
 //Print D
-console.log(`The distance between the points of coordenates (${x1},${y1}) and (${x2},${y2}) is ${D} u.c..)`);
+console.log(`The distance between two points of coordenates (${x1},${y1}) and (${x2},${y2}) is ${D} u.c..`);

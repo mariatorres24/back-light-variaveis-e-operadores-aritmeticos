3ef902e3368ee33260fd271fe42db14a23181fc3
Maylla Rabay <mayllarabay@gmail.com>
2021-03-24 04:13:35 -0300

    Melhorando resposta do console.log

--- m01-a02-extra-14/index.js
+++ m01-a02-extra-14/index.js
@@ -2,4 +2,4 @@ let numeroLados = 4;
 let somaAngulos = (numeroLados - 2) * 180;
 let valorAngulo = somaAngulos / numeroLados;
 
-console.log(`Tendo um polígono de ${numeroLados} lados, a soma de seus ângulos internos é ${somaAngulos} e cada ângulo interno vale ${valorAngulo} graus.`)
+console.log(`Tendo um polígono de ${numeroLados} lados, a soma de seus ângulos internos é ${somaAngulos}. Caso seja um polígono regular (todos os ângulos internos iguais), cada ângulo interno seu vale ${valorAngulo} graus.`)

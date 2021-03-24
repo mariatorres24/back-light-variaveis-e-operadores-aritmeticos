a279456a461afbff14010b4eb6983794e9c86cfb
Maylla Rabay <mayllarabay@gmail.com>
2021-03-24 04:01:27 -0300

    Arrumando os valores das variáveis: de reais para centavos (padrão)

--- m01-a02-classe-08/index.js
+++ m01-a02-classe-08/index.js
@@ -1,5 +1,5 @@
-let valorTenis = 129.99;
-let dinheiroNaMao = 80;
+let valorTenis = 12999; //Em centavos
+let dinheiroNaMao = 8000; //Em centavos
 let desconto = Math.round (100 - ((dinheiroNaMao * 100) / valorTenis));
 
 console.log(`Preciso de ${desconto}% de desconto para comprar o tênis`);

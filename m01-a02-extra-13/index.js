9aa7ec2ab6ffdde021273914490dd47dd4a73866
Danilo Tavares <stavaresdanilo@gamil.com>
2021-03-24 20:50:00 -0300

    Realizado exercício 13

---
+++ m01-a02-extra-13/index.js
@@ -0,0 +1,7 @@
+let r = 3 // Raio em metros;
+
+
+Circ = 2*Math.PI*r;
+Area = Math.PI*Math.pow(r,2);
+
+console.log (`A circunferência do círculo é ${Circ.toFixed(2)} metros com área de ${Area.toFixed(2)} m²`);

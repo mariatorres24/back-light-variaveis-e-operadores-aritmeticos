e1085947e9404063f38386bd82f18645cec8b754
Maylla Rabay <mayllarabay@gmail.com>
2021-03-24 04:06:14 -0300

    Mudando valor da variável capital: de reais para centavos (padrão)

--- m01-a02-casa-09/index.js
+++ m01-a02-casa-09/index.js
@@ -1,6 +1,6 @@
-let capital = 1000;
+let capital = 100000; //Em centavos
 let taxaJuros = 12.5;
 let tempo = 5;
-let montante = (capital * Math.pow((1 + (taxaJuros / 100)), tempo)).toFixed(2);
+let montante = ((capital * Math.pow((1 + (taxaJuros / 100)), tempo)) / 100).toFixed(2); //Em reais
 
-console.log(`Aplicando um capital de R$${capital} por ${tempo} meses à uma taxa mensal fixa de ${taxaJuros}% no sistema de juros compostos, meu montante será de R$${montante}`);
+console.log(`Aplicando um capital de R$${capital / 100} por ${tempo} meses à uma taxa mensal fixa de ${taxaJuros}% no sistema de juros compostos, meu montante será de R$${montante}`);

1b2b3357791216cf8c4a0e66bfd5eafd68f80450
afonsocgl <afonsocgl@gmail.com>
2021-03-24 20:50:56 -0300

    Afonso-Leal-Exercicios-Front-19-03

---
+++ m01-a02-extra-14/index.js
@@ -0,0 +1,10 @@
+let nLadosPoligono = 6, somaAngulos = 0, angulo = 0;
+
+somaAngulos = (nLadosPoligono - 2)*180;
+
+console.log (`A soma dos ângulos internos deste polígono de ${nLadosPoligono} lados, é ${somaAngulos}°`);
+
+angulo = somaAngulos /  nLadosPoligono;
+
+console.log (`Cada ângulo interno deste polígono tem ${angulo}°`);
+

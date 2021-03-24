62bd2ab014f6c726ee4840e08bb2adff925d3c9c
Danilo Tavares <stavaresdanilo@gamil.com>
2021-03-24 20:58:44 -0300

    Realizado exercício 15

---
+++ m01-a02-extra-15/index.js
@@ -0,0 +1,5 @@
+let a =2 , b = 3, c = 3; // Coeficiente da equação do segundo grau Ax² +Bx + C
+
+delta = Math.pow(b,2)-4*a*c;
+
+console.log(`O delta é de ${delta} referente a equação ${a}x²+${b}x+${c}`);

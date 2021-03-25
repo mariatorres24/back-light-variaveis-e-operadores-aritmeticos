afb1582cc94c6304a98fb001a1598dfb2e8d5200
Railane Passos <railanepassos@gmail.com>
2021-03-24 22:08:14 -0300

    Calcular a distância entre dois pontos

---
+++ m01-a02-casa-10/index.js
@@ -0,0 +1,6 @@
+// Calcular distâncias entre dois pontos
+
+let x1 = 1, x2 = 1, y1 = 1, y2 = 4, resultado = 0;
+resultado = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+console.log(resultado);

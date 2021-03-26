d39a2ac0d9f5c018686355f398dd7527df7ea3d6
CarineMorais <cari_morais@hotmail.com>
2021-03-26 18:09:05 -0300

    Correção dos exercicios

--- m01-a02-casa-09/index.js
+++ m01-a02-casa-09/index.js
@@ -1,9 +1,6 @@
-//Aplicando hoje na caderneta de poupança a quantia de R$ 20.000,00, 
-//qual será o montante gerado ao final de 4 anos, sabendo que a rentabilidade mensal é de 0,5%?
-
-const capital = 20000;
-const taxaDeJuros = 0.005;
-const tempo = 48;
+const capital = 1000 ;
+const taxaDeJuros = 0.125;
+const tempo = 5;
 const montante = capital * Math.pow((1 + taxaDeJuros), tempo);
 
-console.log(montante);
+console.log(montante.toFixed(2));
--- m01-a02-classe-08/index.js
+++ m01-a02-classe-08/index.js
@@ -1,9 +1,9 @@
 //multiplicar o valor do produto pela porcentagem dada. 
 //subtrair esse valor do preço inicial do tênis, para saber valor total com o desconto.
 
-const produto = 200;
-const desconto = 10;
+const valorTenis = 129.99;
+const possoGastar = 80;
 
-const descontoTotal = (produto * desconto) - produto;
+const descontoAmigo = (possoGastar / valorTenis) * 100;
 
-console.log(descontoTotal + " " + "reais.");
+console.log(descontoAmigo.toFixed(2) + " " + "de desconto.");
